6ede7e764d1db6e7ab818e11d665691273fb7b97
princetiwari26 <princetiwari.profes@gmail.com>
2025-07-22 23:25:09 +0530

    backend

---
+++ backend/src/main/java/com/princet/questace/QuestAceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.princet.questace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestAceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestAceBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/princet/questace/QuestAceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princet.questace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestAceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
